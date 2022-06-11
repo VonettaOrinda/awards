bc65df8246692e06f5d63e5a542b287222f5aa45
VONETTA <thevonetteway@gmail.com>
2022-06-11 22:24:37 +0300

    updated base.html,home.html,navbar.html

--- awwards/views.py
+++ awwards/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .forms import PostForm
+import random
+from .models import Post
 
-# Create your views here.
+
+
+
+def home(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+    else:
+        form = PostForm()
+
+    try:
+        posts = Post.objects.all()
+        posts = posts[::-1]
+        a_post = random.randint(0, len(posts)-1)
+        random_post = posts[a_post]
+        print(random_post.photo)
+    except Post.DoesNotExist:
+        posts = None
+    return render(request, 'home.html', {'posts': posts, 'form': form, 'random_post': random_post})
